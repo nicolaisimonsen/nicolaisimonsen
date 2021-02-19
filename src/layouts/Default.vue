90c405c44f7921d5d8cdcb1671028347087f2aa9
Nicolai <dev@skaevt.com>
2021-02-19 11:27:15 +0100

    initial commit

---
+++ src/layouts/Default.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="layout">
+
+    <slot/>
+  </div>
+</template>
+
+<static-query>
+query {
+  metadata {
+    siteName
+  }
+}
+</static-query>
+
+<style>
+body {
+  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
+  margin:0;
+  padding:0;
+  line-height: 1.5;
+}
+
+.layout {
+  width: 100vw;
+  height: 100vh;
+  margin: 0 auto;
+  padding-left: 20px;
+  padding-right: 20px;
+  display: flex;
+  flex-flow: row;
+  justify-content: center;
+  align-items: center;
+}
+
+
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,19 @@
+<template>
+  <Layout>
+  <video muted autoplay loop playsinline>
+    <source src="https://media.giphy.com/media/j4q4h9uWKWwnYT1k3Z/source.mov"></source>
+  </video>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Hello, world!",
+  },
+};
+</script>
+
+<style>
+
+</style>
